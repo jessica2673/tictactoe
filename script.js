7315af5cf107c8e5b59815d98e8cac6d6eb22883
Jessica <jessica2003mail@gmail.com>
2022-08-29 10:44:44 -0400

    Finalized project. Fixed and styled restart button

--- script.js
+++ script.js
@@ -5,74 +5,80 @@ const wrapped = (() => {
     const endMssg = document.getElementById("endMssg");
     let gameOver = false;
     let counter = 0;
+    let turn = 0;
+    let form = document.querySelector("form");
+    let player1, player2;
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        start();
+    });  
 
     function start() {
-        let form = document.querySelector("form");
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
+        for(let i = 0; i < 9; i++) {
+            square[i].textContent = "";
+            counter = 0;
+        }
 
-            for(let i = 0; i < 9; i++) {
-                square[i].textContent = "";
-                counter = 0;
-            }
-            endMssg.textContent = "";
-
-            gameOver = false;
-            game();
-        });  
+        turn = 0;
+        endMssg.textContent = "";
+        gameOver = false;
+        player1 = game().player(document.querySelector('input[name=name1]').value, document.querySelector('input[name=choice1]').value);
+        player2 = game().player(document.querySelector('input[name=name2]').value, document.querySelector('input[name=choice2]').value);
+        game();
     }
 
     const game = (() => {
-    const player = (name, choice) => {
-        this.name = name;
-        this.choice = choice;
-        return{name, choice};
-    }
-
-    let player1 = player(document.querySelector('input[name=name1]').value, document.querySelector('input[name=choice1]').value);
-    let player2 = player(document.querySelector('input[name=name2]').value, document.querySelector('input[name=choice2]').value);
-
-    counter = 0;
-    let turn = 0;
-    for(let i = 0; i < 9; i++) {
-        square[i].addEventListener("click", () => {
-            if(square[i].textContent !== "") {}
-            else if(turn === 0 && !gameOver) {
-                display(square[i], player1.choice);
-                turn++;
-                counter++;
-                if(checkWin(square)) {endGame(player1);}
-            } else if (turn === 1 && !gameOver) {
-                display(square[i], player2.choice);
-                turn--;
-                if(checkWin(square)) {endGame(player2);}
-                counter++;
-            }
-
-            if(counter >= 9) {
-                endGame();
-            }
-        });
-    }
-    
-    const display = (square, choice) => {
-        let fill = document.createElement("p");
-        fill.classList.add("fill");
-        fill.textContent = choice;
-        square.appendChild(fill);
-    }
-
-    const endGame = (winner) => {
-        if(winner === undefined) {
-            endMssg.textContent = "The game is a tie!";
-        } else {
-            endMssg.textContent = `The winner is ${winner.name}`;
+        const player = (name, choice) => {
+            this.name = name;
+            this.choice = choice;
+            return{name, choice};
         }
-    }
+
+        counter = 0;
+        turn = 0;
+        for(let i = 0; i < 9; i++) {
+            square[i].addEventListener("click", () => {
+                if(square[i].hasChildNodes()) {}
+                else if(turn === 0 && !gameOver) {
+                    display(square[i], player1.choice);
+                    turn++;
+                    counter++;
+                    if(checkWin(square)) {endGame(player1);}
+                } else if (turn === 1 && !gameOver) {
+                    display(square[i], player2.choice);
+                    turn--;
+                    if(checkWin(square)) {endGame(player2);}
+                    counter++;
+                }
+
+                if(counter >= 9) {
+                    endGame();
+                }
+            });
+        }
+        
+        const display = (square, choice) => {
+            let fill = document.createElement("p");
+            fill.classList.add("fill");
+            fill.textContent = choice;
+            square.appendChild(fill);
+        }
+
+        const endGame = (winner) => {
+            if(winner === undefined) {
+                endMssg.textContent = "The game is a tie!";
+            } else {
+                endMssg.textContent = `The winner is ${winner.name}`;
+            }
+        }
 
         restart.addEventListener("click", () => {
             start();
         });
+
+        return {player}
     })
 
     const checkWin = ((array) => {
@@ -97,10 +103,3 @@ const wrapped = (() => {
         return false;
     }); 
 })();
-
-
-
-
-
-
-
